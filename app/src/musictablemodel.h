3519cf86888c48721f0a860c0d905d616fca5b1f
MSM <nikolayenkoserhiy@gmail.com>
2020-10-25 17:56:39 +0200

    add some menu

--- app/src/musictablemodel.h
+++ app/src/musictablemodel.h
@@ -15,19 +15,24 @@
 class MusicTableModel : public QAbstractTableModel
 {
     Q_OBJECT
+ public:
+  explicit MusicTableModel(QWidget *parent = nullptr);
+  int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+  int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+  QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+  QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+  void sort(int column, Qt::SortOrder order = Qt::AscendingOrder) override;
+  void music_list_add(QVector<QVector<QString>> params);
+
+ private:
     QWidget *m_parent;
     QVector<QString> listHeaders = {"Name", "Time", "Title", "Artist", "Genre", "Album", "Year", "Track"};
     QHash<int, QByteArray> m_roleNames;
-    QVector<QVector<QString>> music_list/*{{"bbb", "BBB", "R&B", "bbb", "BBB", "R&B"}, {"cccaasdasdasasdasdasdasdasddasd", "CCC", "Rock", "ccc", "CCC", "Rock"}, {"aaa", "AAA", "Rock", "aaa", "AAA", "Rock"}, {"Killer", "Queen", "Jass", "aaa", "AAA", "Rock"}}*/;
+    QVector<QVector<QString>> music_list/*{{"bbb", "BBB", "R&B", "bbb", "BBB", "R&B"},
+ *                                          {"ccasd", "CCC", "Rock", "ccc", "CCC", "Rock"},
+ *                                          {"aaa", "AAA", "Rock", "aaa", "AAA", "Rock"},
+ *                                          {"Killer", "Queen", "Jass", "aaa", "AAA", "Rock"}}*/;
 
-public:
-    explicit MusicTableModel(QWidget *parent = nullptr);
-    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
-    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
-    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
-    void sort(int column, Qt::SortOrder order = Qt::AscendingOrder) override;
-    void music_list_add(QVector<QVector<QString>> params);
 };
 
 #endif // MUSICTABLEMODEL_H
